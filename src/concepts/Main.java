070c1573b7db97903eb3972ed9427519fb8ef3a1
Frank Cingel <frankhcingel@gmail.com>
2020-05-03 21:46:50 -0400

    Initial commit. Create .gitignore and main

---
+++ src/concepts/Main.java
@@ -0,0 +1,15 @@
+package concepts;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Type the name of the concept you would like to run:");
+        String concept = input.next();
+        
+
+
+    }
+}
